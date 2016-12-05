fe6d65ee2b02f8252e38000bebd2cfcb5bef0c89
Benjamin White <benjamin.e.white1@gmail.com>
2016-12-05 18:57:00 -0500

    starting point

--- models.js
+++ models.js
@@ -1,57 +1,13 @@
+// you should replace ALL of the code in this file
+// with a Mongo(ose)-backed model for recipes
+
 const uuid = require('uuid');
-
-// this module provides volatile storage, using a `ShoppingList`
-// and `Recipes` model. We haven't learned about databases yet,
-// so for now we're using in-memory storage. This means each time
-// the app stops, our storage gets erased.
-
-// don't worry to much about how `ShoppingList` and `Recipes`
-// are implemented. Our concern in this example is with how
-// the API layer is implemented, and getting it to use an
-// existing model.
-
 
 function StorageException(message) {
    this.message = message;
    this.name = "StorageException";
 }
 
-const ShoppingList = {
-  create: function(name, checked) {
-    console.log('Creating new shopping list item');
-    const item = {
-      name: name,
-      id: uuid.v4(),
-      checked: checked
-    };
-    this.items[item.id] = item;
-    return item;
-  },
-  get: function() {
-    console.log('Retrieving shopping list items');
-    return Object.keys(this.items).map(key => this.items[key]);
-  },
-  delete: function(id) {
-    console.log(`Deleting shopping list item \`${id}\``);
-    delete this.items[id];
-  },
-  update: function(updatedItem) {
-    console.log(`Deleting shopping list item \`${updatedItem.id}\``);
-    const {id} = updatedItem;
-    if (!(id in this.items)) {
-      throw StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.items[updatedItem.id] = updatedItem;
-    return updatedItem;
-  }
-};
-
-function createShoppingList() {
-  const storage = Object.create(ShoppingList);
-  storage.items = {};
-  return storage;
-}
 
 const Recipes = {
   create: function(name, ingredients) {
@@ -84,14 +40,13 @@ const Recipes = {
   }
 };
 
-
+// you won't need an equivalent function  -- we just had this here to get
+// our in-memory models to work
 function createRecipes() {
   const storage = Object.create(Recipes);
   storage.items = {};
   return storage;
 }
 
-module.exports = {
-  ShoppingList: createShoppingList(),
-  Recipes: createRecipes()
-}
+// you'll need to export your `Recipe` model
+module.exports = {Recipe: createRecipes()}
--- server.js
+++ server.js
@@ -1,29 +1,66 @@
-
+const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 
+
 const app = express();
-
-const shoppingListRouter = require('./shoppingListRouter');
-const recipesRouter = require('./recipesRouter');
-
 // log the http layer
 app.use(morgan('common'));
 
-app.use(express.static('public'));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+const {PORT} = require('./config');
+const {Recipe} = require('./models');
+
+// manually add some recipes so we have some data
+// once you update `Recipe` to use Mongoose, you'll
+// need to update these
+Recipe.create('chocolate milk', ['cocoa', 'milk']);
+Recipe.create('grilled cheese', ['bread', 'cheese', 'buter']);
+
+// the four CRUD routes below need to be updated to use
+// Mongoose-backed `Recipe` model
+app.post('/recipes', (req, res) => {
+  const item = Recipe.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
+app.get('/recipes', (req, res) => {
+  res.json({recipes: Recipe.get()});
+});
+
+app.put('/recipes/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({message: message});
+  }
+  console.log(`Updating recipe item \`${req.params.id}\``);
+  const updatedItem = Recipe.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).json(updatedItem);
+})
+
+// Delete recipes (by id)!
+app.delete('/recipes/:id', (req, res) => {
+  Recipe.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.ID}\``);
+  res.status(204).end();
 });
 
 
-// when requests come into `/shopping-list` or
-// `/recipes`, we'll route them to the express
-// router instances we've imported. Remember,
-// these router instances act as modular, mini-express apps.
-app.use('/shopping-list', shoppingListRouter);
-app.use('/recipes', recipesRouter);
-
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+// catch-all endpoint if client makes request to non-existent endpoint
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
 });
+
+
+app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}`));
+
+
+module.exports = {app};
