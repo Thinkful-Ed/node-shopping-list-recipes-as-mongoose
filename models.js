7bb457e46823a5ff0c2a0544523779146e14c75d
Benjamin White <benjamin.e.white1@gmail.com>
2016-12-05 19:33:28 -0500

    working solution

--- models.js
+++ models.js
@@ -1,52 +1,12 @@
-// you should replace ALL of the code in this file
-// with a Mongo(ose)-backed model for recipes
+const mongoose = require('mongoose');
 
-const uuid = require('uuid');
+const recipeSchema = mongoose.Schema({
+  name: String,
+  ingredients: [String]
+});
 
-function StorageException(message) {
-   this.message = message;
-   this.name = "StorageException";
-}
+const Recipe = mongoose.model('Recipe', recipeSchema);
 
-
-const Recipes = {
-  create: function(name, ingredients) {
-    console.log('Creating a new recipe');
-    const item = {
-      name: name,
-      id: uuid.v4(),
-      ingredients: ingredients
-    };
-    this.items[item.id] = item;
-    return item;
-  },
-  get: function() {
-    console.log('Retreiving recipes');
-    return Object.keys(this.items).map(key => this.items[key]);
-  },
-  delete: function(itemId) {
-    console.log(`Deleting recipe with id \`${itemId}\``);
-    delete this.items[itemId];
-  },
-  update: function(updatedItem) {
-    console.log(`Updating recipe with id \`${updatedItem.id}\``);
-    const {id} = updatedItem;
-    if (!(id in this.items)) {
-      throw StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.items[updatedItem.id] = updatedItem;
-    return updatedItem;
-  }
-};
-
-// you won't need an equivalent function  -- we just had this here to get
-// our in-memory models to work
-function createRecipes() {
-  const storage = Object.create(Recipes);
-  storage.items = {};
-  return storage;
-}
 
 // you'll need to export your `Recipe` model
-module.exports = {Recipe: createRecipes()}
+module.exports = {Recipe}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 
@@ -9,48 +10,71 @@ app.use(morgan('common'));
 
 app.use(bodyParser.json());
 
-const {PORT} = require('./config');
+const {PORT, DATABASE_URL} = require('./config');
 const {Recipe} = require('./models');
 
-// manually add some recipes so we have some data
-// once you update `Recipe` to use Mongoose, you'll
-// need to update these
-Recipe.create('chocolate milk', ['cocoa', 'milk']);
-Recipe.create('grilled cheese', ['bread', 'cheese', 'buter']);
-
-// the four CRUD routes below need to be updated to use
-// Mongoose-backed `Recipe` model
-app.post('/recipes', (req, res) => {
-  const item = Recipe.create(req.body.name, req.body.ingredients);
-  res.status(201).json(item);
-});
-
 app.get('/recipes', (req, res) => {
-  res.json({recipes: Recipe.get()});
+  Recipe
+    .find()
+    .exec()
+    .then(
+      // success callback
+      recipes => res.json({recipes}),
+      // failure callback
+      err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+      }
+    )}
+);
+
+// can also request by ID
+app.get('/recipes/:id', (req, res) => {
+  Recipe
+    .findById(req.params.id)
+    .exec()
+    .then(
+      recipe => res.json(recipe),
+      err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'})}
+    );
 });
+
+
+app.post('/recipes', (req, res) => {
+  const {name, ingredients} = req.body;
+
+  Recipe
+    .create({name, ingredients})
+    .then(
+      recipe => res.status(201).json(recipe),
+      err => res.status(500).json({message: 'Internal server error'}));
+});
+
 
 app.put('/recipes/:id', (req, res) => {
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message = (
-      `Request path id (${req.params.id}) and request body id ` +
+      `Request path id (${req.params.id}) and request body id `
       `(${req.body.id}) must match`);
     console.error(message);
-    return res.status(400).json({message: message});
+    return res.status(400).send(message);
   }
-  console.log(`Updating recipe item \`${req.params.id}\``);
-  const updatedItem = Recipe.update({
-    id: req.params.id,
-    name: req.body.name,
-    ingredients: req.body.ingredients
-  });
-  res.status(204).json(updatedItem);
-})
+  Recipe
+    .findByIdAndUpdate(req.params.id, {
+      name: req.body.name, ingredients: req.body.ingredients})
+    .then(
+      recipe => res.status(204).end(),
+      err => res.status(500).json({message: 'Internal server error'}));
+});
 
-// Delete recipes (by id)!
 app.delete('/recipes/:id', (req, res) => {
-  Recipe.delete(req.params.id);
-  console.log(`Deleted shopping list item \`${req.params.ID}\``);
-  res.status(204).end();
+  Recipe
+    .findByIdAndRemove(req.params.id)
+    .then(
+      () => res.status(204).end(),
+      err => res.status(500).json({message: 'Internal server error'}));
 });
 
 
@@ -59,8 +83,32 @@ app.use('*', function(req, res) {
   res.status(404).json({message: 'Not Found'});
 });
 
+// this function connects to our database, then starts the server
+function runServer(callback) {
+  mongoose.connect(DATABASE_URL, (err) => {
+    if (err && callback) {
+      return callback(err);
+    }
 
-app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}`));
+    app.listen(PORT, () => {
+      console.log(`Your app is listening on port ${PORT}`);
+      if (callback) {
+        callback();
+      }
+    });
+  });
+};
 
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance,
+// test code) can start the server as needed.
+if (require.main === module) {
+  runServer(function(err) {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
 
-module.exports = {app};
+module.exports = {app, runServer};
+
